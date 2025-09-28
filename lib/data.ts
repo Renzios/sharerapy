b8bf797bcafbb031c6a0d9afc793d24e0ae28f88
Renzios <61353378+Renzios@users.noreply.github.com.>
2025-09-29 02:17:49 +0800

    feat: add filter and sort

--- lib/data.ts
+++ lib/data.ts
@@ -1,12 +1,19 @@
 import { createClient } from '@/lib/supabase/server';
 
-export async function readPatients() {
+export async function readPatients(ascending: boolean, countryID?: number, sex?: 'Male' | 'Female') {
     const supabase = await createClient();
 
-    const { data, error } = await supabase
+    let query = supabase
         .from('patients_view')
         .select('*, country:countries(*)');
+    
+    if (countryID) query = query.eq('country_id', countryID);
+    if (sex) query = query.eq('sex', sex);
 
+    query = query.order('name', { ascending });
+
+    const { data, error } = await query;
+        
     if (error) throw error;
 
     return data;
@@ -26,12 +33,18 @@ export async function readPatient(id: string) {
     return data;
 }
 
-export async function readTherapists() {
+export async function readTherapists(ascending: boolean, clinicID?: number) {
     const supabase = await createClient();
 
-    const { data, error } = await supabase
-        .from('therapists')
+    let query = supabase
+        .from('therapists_view')
         .select('*, clinic:clinics(*, country:countries(*))');
+
+    if (clinicID) query = query.eq('clinic_id', clinicID);
+
+    query = query.order('name', { ascending });
+
+    const { data, error } = await query
     
     if (error) throw error;
 
@@ -42,7 +55,7 @@ export async function readTherapist(id: string) {
     const supabase = await createClient();
 
     const { data, error } = await supabase
-        .from('therapists')
+        .from('therapists_view')
         .select('*, clinic:clinics(*, country:countries(*))')
         .eq('id', id)
         .single();
@@ -52,13 +65,24 @@ export async function readTherapist(id: string) {
     return data;
 }
 
-export async function readReports() {
+export async function readReports(sortBy: 'title' | 'created_at', ascending: boolean, countryID?: number, languageID?: number, typeIDs?: number[], clinicID?: number, startDate?: string, endDate?: string, therapistID?: string) {
     const supabase = await createClient();
 
-    const { data, error } = await supabase
+    let query = supabase
         .from('reports')
-        .select('*, therapist:therapists(*, clinic:clinics(*, country:countries(*))), type:types(*), language:languages(*), patient:patients_view(*, country:countries(*))');
+        .select('*, therapist:therapists!inner(*, clinic:clinics!inner(*, country:countries(*))), type:types(*), language:languages(*), patient:patients_view(*, country:countries(*))');
     
+    if (languageID) query = query.eq('language_id', languageID);
+    if (countryID) query = query.eq('therapist.clinic.country_id', countryID);
+    if (typeIDs?.length) query = query.in('type_id', typeIDs);
+    if (clinicID) query = query.eq('therapist.clinic_id', clinicID);
+    if (startDate) query = query.gte('created_at', startDate);
+    if (endDate) query = query.lte('created_at', endDate);
+    if (therapistID) query = query.eq('therapist_id', therapistID);
+
+    query = query.order(sortBy, { ascending });
+
+    const { data, error } = await query
     if (error) throw error;
 
     return data;
@@ -69,7 +93,7 @@ export async function readReport(id: string) {
 
     const { data, error } = await supabase
         .from('reports')
-        .select('*, therapist:therapists(*, clinic:clinics(*, country:countries(*))), type:types(*), language:languages(*), patient:patients_view(*, country:countries(*))')
+        .select('*, therapist:therapists!inner(*, clinic:clinics!inner(*, country:countries(*))), type:types(*), language:languages(*), patient:patients_view(*, country:countries(*))')
         .eq('id', id)
         .single();
     
@@ -78,13 +102,19 @@ export async function readReport(id: string) {
     return data;
 }
 
-export async function readClinics() {
+export async function readClinics(countryID?: number) {
     const supabase = await createClient();
 
-    const { data, error } = await supabase
+    let query = supabase
         .from('clinics')
-        .select('*, country:countries(*)');
+        .select('*, country:countries(*)')
+
+    if (countryID) query = query.eq('country_id', countryID);
+
+    query = query.order('clinic', { ascending: true });
     
+    const { data, error } = await query;
+
     if (error) throw error;
 
     return data;
@@ -95,7 +125,8 @@ export async function readCountries() {
 
     const { data, error } = await supabase
         .from('countries')
-        .select('*');
+        .select('*')
+        .order('country', { ascending: true });
     
     if (error) throw error;
 
@@ -107,7 +138,8 @@ export async function readLanguages() {
 
     const { data, error } = await supabase
         .from('languages')
-        .select('*');
+        .select('*')
+        .order('language', { ascending: true });
     
     if (error) throw error;
 
@@ -119,7 +151,8 @@ export async function readTypes() {
 
     const { data, error } = await supabase
         .from('types')
-        .select('*');
+        .select('*')
+        .order('type', { ascending: true });
     
     if (error) throw error;
 
