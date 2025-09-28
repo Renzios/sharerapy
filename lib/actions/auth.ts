23ac3e8462103c1e275a4ed85a302fc13e43d2cb
Renzios <61353378+Renzios@users.noreply.github.com.>
2025-09-28 15:37:52 +0800

    feat: add server actions for login, signup, and logout

---
+++ lib/actions/auth.ts
@@ -0,0 +1,45 @@
+'use server'
+
+import { createClient } from '@/lib/supabase/server'
+
+export async function login(formData: FormData) {
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function signOut() {
+  const supabase = await createClient()
+
+  const { error } = await supabase.auth.signOut()
+}
