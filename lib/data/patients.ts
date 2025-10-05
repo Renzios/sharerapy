1e99b2bb6d89707e8d7c5e15c66272e237d14614
Dawsxn <dawscatignas008@gmail.com>
2025-10-05 17:58:31 +0800

    fix: changed default pageSize param for readPatients

--- lib/data/patients.ts
+++ lib/data/patients.ts
@@ -1,38 +1,38 @@
-import { createClient } from '@/lib/supabase/server';
-import { ReadParameters } from '@/lib/types/types';
+import { createClient } from "@/lib/supabase/server";
+import { ReadParameters } from "@/lib/types/types";
 
 export async function readPatients({
-    ascending = true,
-    countryID,
-    sex,
-    page = 0,
-    pageSize = 10
+  ascending = true,
+  countryID,
+  sex,
+  page = 0,
+  pageSize = 20,
 }: ReadParameters = {}) {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    const query = supabase
-        .from('patients_view')
-        .select('*, country:countries(*)', { count: 'exact' })
-        .order('name', { ascending })
-        .range(page * pageSize, page * pageSize + pageSize - 1);
-    
-    if (countryID) query.eq('country_id', countryID);
-    if (sex) query.eq('sex', sex);
+  const query = supabase
+    .from("patients_view")
+    .select("*, country:countries(*)", { count: "exact" })
+    .order("name", { ascending })
+    .range(page * pageSize, page * pageSize + pageSize - 1);
 
-    const { data, error, count } = await query;
-    if (error) throw error;
-    return { data, count };
+  if (countryID) query.eq("country_id", countryID);
+  if (sex) query.eq("sex", sex);
+
+  const { data, error, count } = await query;
+  if (error) throw error;
+  return { data, count };
 }
 
 export async function readPatient(id: string) {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    const { data, error } = await supabase
-        .from('patients_view')
-        .select('*, country:countries(*)')
-        .eq('id', id)
-        .single();
+  const { data, error } = await supabase
+    .from("patients_view")
+    .select("*, country:countries(*)")
+    .eq("id", id)
+    .single();
 
-    if (error) throw error;
-    return data;
+  if (error) throw error;
+  return data;
 }
