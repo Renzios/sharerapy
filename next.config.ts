290e1ea790ac6e5328633354f266410cd189d4bf
Dawsxn <dawscatignas008@gmail.com>
2025-10-05 20:29:31 +0800

    chore: added image compatibility in next config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,16 @@ const nextConfig: NextConfig = {
   devIndicators: {
     position: "bottom-right",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
