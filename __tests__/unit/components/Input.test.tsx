9b17b7d7a2fba703aef27219c75420429fbff5cf
Anthony Baybayon <anthony_baybayon@dlsu.edu.ph>
2025-10-09 22:57:41 +0800

    chore: renamed InputField to Input

--- __tests__/unit/components/Input.test.tsx
+++ __tests__/unit/components/Input.test.tsx
@@ -1,21 +1,21 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import InputField from '@/components/InputField';
+import Input from '@/components/Input';
 
-describe('InputField Component', () => {
+describe('Input Component', () => {
   it('renders correctly', () => {
-    render(<InputField />);
+    render(<Input />);
     const input = screen.getByRole('textbox');
     expect(input).toBeInTheDocument();
   });
 
   it('passes the maxLength prop to the input', () => {
-    render(<InputField maxLength={10} />);
+    render(<Input maxLength={10} />);
     const input = screen.getByRole('textbox');
     expect(input).toHaveAttribute('maxLength', '10');
   });
 
   it('limits input manually if implemented', () => {
-    render(<InputField maxLength={5} />);
+    render(<Input maxLength={5} />);
     const input = screen.getByRole('textbox');
     fireEvent.change(input, { target: { value: '123456' } });
     expect(input.value.length).toBeLessThanOrEqual(5);
