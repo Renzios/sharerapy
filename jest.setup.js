39777e369340d319ce8a9f4a623db188598b67eb
marieltmdg <marielmollytamondong@gmail.com>
2025-10-14 10:01:34 +0800

    ci: align to CRUD testing

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,19 @@
 import '@testing-library/jest-dom'
+
+// Basic polyfills for Node.js globals in jsdom environment
+if (typeof global.TextEncoder === 'undefined') {
+  const { TextEncoder, TextDecoder } = require('util')
+  global.TextEncoder = TextEncoder
+  global.TextDecoder = TextDecoder
+}
+
+// Mock Next.js navigation for component tests
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(() => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    prefetch: jest.fn(),
+  })),
+  useSearchParams: jest.fn(() => new URLSearchParams()),
+  usePathname: jest.fn(() => '/'),
+}))
