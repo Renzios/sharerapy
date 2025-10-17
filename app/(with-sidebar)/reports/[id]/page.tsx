606ae8960ddf5bed2ec8c5dc61f833351ee1ba13
Dawsxn <dawscatignas008@gmail.com>
2025-10-18 05:23:53 +0800

    feat: connect individual report pages to db

--- app/(with-sidebar)/reports/[id]/page.tsx
+++ app/(with-sidebar)/reports/[id]/page.tsx
@@ -1,6 +1,20 @@
+import IndivReportClient from "@/components/client-pages/IndivReportClient";
+import { readReport } from "@/lib/data/reports";
+
 /**
- * View individual report page depending on dynamic id
+ * This is the server component for viewing an individual report.
+ * It fetches the report data by ID and passes it to the client component for rendering.
  */
-export default function ViewIndividualReportPage() {
-  return <div></div>;
+export default async function ViewIndividualReportPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const report = await readReport(params.id);
+
+  return (
+    <div>
+      <IndivReportClient report={report} />
+    </div>
+  );
 }
