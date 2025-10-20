c97676dba1192270e130883a2c976084bccccedf
Dawsxn <dawscatignas008@gmail.com>
2025-10-21 05:54:36 +0800

    feat: revise ReportCard design

--- components/cards/ReportCard.tsx
+++ components/cards/ReportCard.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { getPublicURL } from "@/lib/utils/storage";
+import { formatDistanceToNow } from "date-fns";
+import Tag from "@/components/general/Tag";
 
 interface ReportCardProps {
   report: {
@@ -31,10 +33,10 @@ interface ReportCardProps {
 }
 
 export default function ReportCard({ report }: ReportCardProps) {
-  // Format the date
-  const dateUploaded = new Date(report.created_at).toLocaleDateString();
+  const dateUploaded = formatDistanceToNow(new Date(report.created_at), {
+    addSuffix: true, // ago
+  });
 
-  // Construct display values from nested data
   const therapistName = `${report.therapist.first_name} ${report.therapist.last_name}`;
   const country = report.therapist.clinic.country.country;
   const clinic = report.therapist.clinic.clinic;
@@ -45,46 +47,51 @@ export default function ReportCard({ report }: ReportCardProps) {
     <Link
       href={`/reports/${report.id}`}
       className="
+        group
         flex flex-col gap-y-2
         bg-white rounded-[0.5rem] p-6
         border border-bordergray
         hover:bg-bordergray/30 hover:cursor-pointer
         transition-transform duration-200 ease-in-out
- 
-        "
+      "
     >
-      <div className="flex">
-        <div className="mb-3">
-          <h1 className="font-Noto-Sans text-xl text-black font-semibold">
-            {report.title}
-          </h1>
-          <p className="font-Noto-Sans text-[0.6875rem] font-medium text-darkgray ml-0.5">
-            {`${dateUploaded} | ${country} | ${language} | ${therapyType} | ${clinic}`}
+      <div className="flex items-center gap-x-2 mb-2">
+        <Image
+          src={getPublicURL(
+            "therapist_pictures",
+            report.therapist.picture || ""
+          )}
+          alt="Therapist Profile Picture"
+          width={100}
+          height={100}
+          className="rounded-full object-cover h-[2rem] w-[2rem]"
+        />
+        <div className="flex gap-x-2">
+          <p className="font-Noto-Sans text-sm text-darkgray font-medium">
+            Written by {therapistName}
+          </p>
+          <p className="font-Noto-Sans text-sm text-darkgray font-medium">•</p>
+          <p className="font-Noto-Sans text-sm text-darkgray font-medium">
+            {dateUploaded}
           </p>
         </div>
-        <div className="ml-auto gap-x-2 hidden md:flex">
-          <div className="flex items-center pb-3">
-            <h2 className="font-Noto-Sans text-sm text-primary text-right hover:underline">
-              {therapistName}
-            </h2>
-          </div>
-          <div className="flex">
-            <Image
-              src={getPublicURL(
-                "therapist_pictures",
-                report.therapist.picture || ""
-              )}
-              alt="Therapist Profile Picture"
-              width={100}
-              height={100}
-              className="rounded-full object-cover h-[2.5rem] w-[2.5rem]"
-            />
+      </div>
+      <div className="flex">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="font-Noto-Sans text-lg md:text-xl text-black font-semibold">
+            {report.title}
+          </h1>
+          <div className="flex flex-wrap gap-2">
+            <Tag text={country} fontSize="text-xs" />
+            <Tag text={language} fontSize="text-xs" />
+            <Tag text={therapyType} fontSize="text-xs" />
+            <Tag text={clinic} fontSize="text-xs" />
           </div>
         </div>
       </div>
 
-      <div className="mb-3">
-        <p className="font-Noto-Sans text-sm text-darkgray">
+      <div className="mt-3 mb-3">
+        <p className="font-Noto-Sans text-sm text-darkgray line-clamp-2">
           {report.description}
         </p>
       </div>
