e54f8c4df5dfaccbb955cad7039321998c04bfe3
Dawsxn <dawscatignas008@gmail.com>
2025-11-08 00:01:10 +0800

    fix: exclude not-found pages during build

--- middleware.ts
+++ middleware.ts
@@ -13,8 +13,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
+     * - _not-found (Next.js internal error page)
+     * - api (API routes)
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|_not-found|api|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
