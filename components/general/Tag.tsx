61cb1c30cc9138198ed83a62755c966941bd221f
Dawsxn <dawscatignas008@gmail.com>
2025-11-10 19:48:32 +0800

    fix: address tag hover transition in PatientCard

--- components/general/Tag.tsx
+++ components/general/Tag.tsx
@@ -12,7 +12,7 @@ export default function Tag({
   className = "",
 }: TagProps) {
   const baseClasses =
-    "flex items-center justify-center rounded-full bg-gray-100 px-3 py-1 font-Noto-Sans font-medium text-black transition-colors duration-200 group-hover:bg-white"; // <-- MODIFIED THIS LINE
+    "flex items-center justify-center rounded-full bg-gray-100 px-3 py-1 font-Noto-Sans font-medium text-black transition-colors duration-200 group-hover:bg-white";
 
   const combinedClasses = [baseClasses, fontSize, className].join(" ").trim();
 
